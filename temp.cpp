dae683a6915fa08daa1f48e4ad721a053ca544d3
wenke1020 <wenkzhao@fudan.edu.cn>
2018-05-15 12:09:05 +0800

    Create temp.cpp

---
+++ temp.cpp
@@ -0,0 +1,18 @@
+// this is the example code for the project...
+
+#include <string>
+
+#include "common/example.cpp"
+
+
+using namespace std;
+
+namespace example
+{
+
+int main
+{
+  cout << "this is the example only !" << endl; 
+}
+
+} // namespace example
